6368058c503edd444ae2bee0380684f365f694bf
N000101\Amily Chang <aymaiclhyu777@gmail.com>
2023-06-14 15:30:11 +0800

    feat: add mongoose

---
+++ models/test.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const testSchema = new Schema({
+  info: {
+    name: {
+      type: String, 
+      required: true // 這是個必填欄位
+    },
+    gender: {
+      type: String,
+      required: false 
+    },
+    age: {
+      type: String, 
+      required: false 
+    },
+    phone: {
+      type: String, 
+      required: false 
+    },
+    email: {
+      type: String, 
+      required: false 
+    },
+  },
+  myScores: {
+    type: Number,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Test', testSchema)
